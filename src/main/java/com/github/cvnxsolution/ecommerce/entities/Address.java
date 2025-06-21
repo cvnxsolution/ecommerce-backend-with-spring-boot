d765cf5364dff980d898a4f502b4974ddf0f25b8
zinminthet <cvnxsolution@gmail.com>
2025-06-21 23:10:00 +0630

    wip(entity): add initial address entity class

--- src/main/java/com/github/cvnxsolution/ecommerce/entities/Address.java
+++ src/main/java/com/github/cvnxsolution/ecommerce/entities/Address.java
@@ -1,4 +1,9 @@
 package com.github.cvnxsolution.ecommerce.entities;
 
 public class Address {
+    private Long id;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
 }
