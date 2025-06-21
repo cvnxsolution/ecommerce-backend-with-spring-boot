7a9574fd4cf9701840db577490503eeacfa48eb2
zinminthet <cvnxsolution@gmail.com>
2025-06-21 23:09:26 +0630

    wip(entity): add initial product entity class

--- src/main/java/com/github/cvnxsolution/ecommerce/entities/Product.java
+++ src/main/java/com/github/cvnxsolution/ecommerce/entities/Product.java
@@ -1,4 +1,10 @@
 package com.github.cvnxsolution.ecommerce.entities;
 
+import java.math.BigDecimal;
+
 public class Product {
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
 }
