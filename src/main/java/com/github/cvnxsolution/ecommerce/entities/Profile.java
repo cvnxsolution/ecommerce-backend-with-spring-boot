381f56704809b6d123a75d07d710c3e83518eb64
zinminthet <cvnxsolution@gmail.com>
2025-06-21 23:09:34 +0630

    wip(entity): add initial profile entity class

--- src/main/java/com/github/cvnxsolution/ecommerce/entities/Profile.java
+++ src/main/java/com/github/cvnxsolution/ecommerce/entities/Profile.java
@@ -1,4 +1,11 @@
 package com.github.cvnxsolution.ecommerce.entities;
 
+import java.sql.Date;
+
 public class Profile {
+    private Long id;
+    private String bio;
+    private String phoneNumber;
+    private Date dateOfBirth;
+    private Byte loyaltyPoints;
 }
