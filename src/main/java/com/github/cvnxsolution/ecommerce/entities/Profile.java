53a7c170eca8997f1dfc2e7c053729788a616411
zinminthet <cvnxsolution@gmail.com>
2025-06-22 21:20:05 +0630

    feat(profile): establish unidirectional one-to-one with user using shared primary key

--- src/main/java/com/github/cvnxsolution/ecommerce/entities/Profile.java
+++ src/main/java/com/github/cvnxsolution/ecommerce/entities/Profile.java
@@ -1,11 +1,39 @@
 package com.github.cvnxsolution.ecommerce.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.sql.Date;
 
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "profiles")
 public class Profile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "bio")
     private String bio;
+
+    @Column(name = "phone_number")
     private String phoneNumber;
+
+    @Column(name = "date_of_birth")
     private Date dateOfBirth;
+
+    @Column(name = "loyalty_points")
     private Byte loyaltyPoints;
+
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    @JoinColumn(name = "id")
+    @MapsId
+    private User user;
 }
--- src/main/java/com/github/cvnxsolution/ecommerce/entities/User.java
+++ src/main/java/com/github/cvnxsolution/ecommerce/entities/User.java
@@ -1,9 +1,45 @@
 package com.github.cvnxsolution.ecommerce.entities;
 
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@ToString
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "password")
     private String password;
+
+    @ManyToMany
+    @JoinTable(name = "users_tags", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "tag_id"))
+    private Set<Tag> tags = new HashSet<>();
+
+    @OneToMany(mappedBy = "user", orphanRemoval = true, cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    @Builder.Default
+    private Set<Address> addresses = new HashSet<>();
+
+    public void addAddress(Address address){
+        addresses.add(address);
+        address.addUser(this);
+    }
 }
