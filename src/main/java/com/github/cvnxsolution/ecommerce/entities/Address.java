02b9259c923f5698bbf68088d7146888d9f10ea1
zinminthet <cvnxsolution@gmail.com>
2025-06-22 21:20:47 +0630

    feat(user): add bidirectional one-to-many relation with address and helper method

--- src/main/java/com/github/cvnxsolution/ecommerce/entities/Address.java
+++ src/main/java/com/github/cvnxsolution/ecommerce/entities/Address.java
@@ -1,9 +1,51 @@
 package com.github.cvnxsolution.ecommerce.entities;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Getter
+@Setter
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "addresses")
 public class Address {
+
+    public Address(Long id){
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, name = "id")
     private Long id;
+
+    @Column(name = "street")
     private String street;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "state")
     private String state;
+
+    @Column(name = "zip")
     private String zip;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @ToString.Exclude
+    private User user;
+
+    public void addUser(User user){
+        this.user = user;
+    }
+
+    public void removeUser(){
+        this.user = null;
+    }
 }
