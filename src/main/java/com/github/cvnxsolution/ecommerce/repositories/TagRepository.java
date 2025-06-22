ec0dcfbbf556af1b67e3fd64aaf741371965d6c9
zinminthet <cvnxsolution@gmail.com>
2025-06-22 23:19:10 +0630

    feat(tag-repo): add query to find tags linked to user email (many-to-many derived query)

--- src/main/java/com/github/cvnxsolution/ecommerce/repositories/TagRepository.java
+++ src/main/java/com/github/cvnxsolution/ecommerce/repositories/TagRepository.java
@@ -3,5 +3,9 @@ package com.github.cvnxsolution.ecommerce.repositories;
 import com.github.cvnxsolution.ecommerce.entities.Tag;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TagRepository extends CrudRepository<Tag, Long> {
+
+    List<Tag> findByUsers_Email(String email);
 }
