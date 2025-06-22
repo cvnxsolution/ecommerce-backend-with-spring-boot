1f4662909e655cc7943035bbd041ffb3abf75426
zinminthet <cvnxsolution@gmail.com>
2025-06-22 23:18:51 +0630

    feat(profile-repo): add query for fetching profile by user email

--- src/main/java/com/github/cvnxsolution/ecommerce/repositories/ProfileRepository.java
+++ src/main/java/com/github/cvnxsolution/ecommerce/repositories/ProfileRepository.java
@@ -1,7 +1,9 @@
 package com.github.cvnxsolution.ecommerce.repositories;
 
 import com.github.cvnxsolution.ecommerce.entities.Profile;
+import com.github.cvnxsolution.ecommerce.entities.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProfileRepository extends CrudRepository<Profile, Long> {
+    Profile findByUser_Email(String email);
 }
