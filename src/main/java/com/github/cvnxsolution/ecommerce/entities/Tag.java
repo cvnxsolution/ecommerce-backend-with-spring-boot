4671e3b1857f1bff98019ec4721f3daf680ae12d
zinminthet <cvnxsolution@gmail.com>
2025-06-22 21:20:52 +0630

    feat(user-tag): implement bidirectional many-to-many relationship via join table

--- src/main/java/com/github/cvnxsolution/ecommerce/entities/Tag.java
+++ src/main/java/com/github/cvnxsolution/ecommerce/entities/Tag.java
@@ -1,6 +1,32 @@
 package com.github.cvnxsolution.ecommerce.entities;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Builder
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tags")
 public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    @Builder.Default
+    @ToString.Exclude
+    private Set<User> users = new HashSet<>();
 }
