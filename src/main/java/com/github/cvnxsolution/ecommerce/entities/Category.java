66bd283a7ad6ec8cce24c1034a41b9d819744ba7
zinminthet <cvnxsolution@gmail.com>
2025-06-22 21:21:00 +0630

    feat(category): set up bidirectional one-to-many relation with product

--- src/main/java/com/github/cvnxsolution/ecommerce/entities/Category.java
+++ src/main/java/com/github/cvnxsolution/ecommerce/entities/Category.java
@@ -1,6 +1,33 @@
 package com.github.cvnxsolution.ecommerce.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Table(name = "categories")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Byte id;
+
+    @Column(name = "name")
     private String name;
+
+
+    @OneToMany(mappedBy = "category", orphanRemoval = true)
+    @Builder.Default
+    private Set<Product> products = new HashSet<>();
+
 }
--- src/main/java/com/github/cvnxsolution/ecommerce/entities/Product.java
+++ src/main/java/com/github/cvnxsolution/ecommerce/entities/Product.java
@@ -1,10 +1,38 @@
 package com.github.cvnxsolution.ecommerce.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.math.BigDecimal;
 
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "price")
     private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    @ToString.Exclude
+    private Category category;
 }
+
+
