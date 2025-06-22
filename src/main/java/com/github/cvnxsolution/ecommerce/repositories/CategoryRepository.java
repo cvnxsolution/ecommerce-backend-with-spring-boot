65a925f45421e9065560ad2a6d0c483b340b607f
zinminthet <cvnxsolution@gmail.com>
2025-06-22 23:19:02 +0630

    feat(category-repo): implement fetch join for eager product loading by category name

--- src/main/java/com/github/cvnxsolution/ecommerce/repositories/CategoryRepository.java
+++ src/main/java/com/github/cvnxsolution/ecommerce/repositories/CategoryRepository.java
@@ -1,4 +1,16 @@
 package com.github.cvnxsolution.ecommerce.repositories;
 
-public class CategoryRepository {
+import com.github.cvnxsolution.ecommerce.entities.Category;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    @Query("select c from Category c join fetch c.products where c.name = :name")
+    Category findByName(@Param("name") String name);
+
+
 }
